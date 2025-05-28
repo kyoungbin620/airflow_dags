de1eaa4e6a78351eb6125a94c064b5dda50182c2
kyoungbin jin <kbjin@mycreatz.com>
2025-05-28 10:07:31 +0900

    fix

--- logs_to_parquet.py
+++ logs_to_parquet.py
@@ -3,6 +3,7 @@ from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import Kubernete
 from airflow.utils.dates import days_ago
 from datetime import timedelta
 from kubernetes.client import models as k8s
+from kubernetes.client import V1ResourceRequirements
 
 dag_name = "log_to_parquet_s3_script"
 
