4db75e98766636c257928dcf4c8960d5f341bf2b
kyoungbin jin <kbjin@mycreatz.com>
2025-05-29 15:10:14 +0900

    fix

--- shot_pipeline/raw_to_parquet.py
+++ shot_pipeline/raw_to_parquet.py
@@ -61,8 +61,8 @@ def raw_to_parquet_dag():
         "--conf", "spark.kubernetes.file.upload.path=local:///opt/spark/tmp",
 
         # Python 의존성(zip) 및 애플리케이션 리소스
-        "--py-files", "s3a://creatz-airflow-jobs/shot_data/zips/raw_to_parquet_v1.0.0.zip",
-        "s3a://creatz-airflow-jobs/shot_data/zips/raw_to_parquet_v1.0.0.zip#main.py",  # ZIP 내부 파일 지정
+        "--py-files", "s3://creatz-airflow-jobs/raw_to_parquet/zips/dependencies.zip",
+        "s3://creatz-airflow-jobs/raw_to_parquet/scripts/run_raw_to_parquet.py",
 
         # 사용자 파라미터
         "--start-date", "{{ params.start_date }}",
