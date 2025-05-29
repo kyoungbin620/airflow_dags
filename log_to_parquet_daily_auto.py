7977935de7cdbb25e6c1149ee355ea18453cb3d2
kyoungbin jin <kbjin@mycreatz.com>
2025-05-29 09:33:21 +0900

    udpate

--- log_to_parquet_daily_auto.py
+++ log_to_parquet_daily_auto.py
@@ -1,6 +1,7 @@
 from airflow import DAG
-from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
+from airflow.decorators import task
 from airflow.utils.dates import datetime, timedelta
+from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
 from kubernetes.client import models as k8s
 from kubernetes.client import V1ResourceRequirements
 
@@ -16,46 +17,50 @@ default_args = {
 with DAG(
     dag_id=dag_name,
     default_args=default_args,
-    start_date=datetime(2025, 5, 28),  # 시작 날짜 적절히 설정
-    schedule_interval="0 2 * * *",  # 매일 새벽 2시
+    start_date=datetime(2025, 5, 28),
+    schedule_interval="0 2 * * *",
     catchup=False,
+    tags=["spark", "s3", "parquet"],
 ) as dag:
 
-    spark_submit = KubernetesPodOperator(
-        task_id="run_spark_submit_s3_script_daily",
-        name="spark-submit-s3-script-daily",
-        namespace="airflow",
-        image=spark_image,
-        cmds=["/opt/spark/bin/spark-submit"],
-        arguments=[
-            "--master", "k8s://https://BFDDB67D4B8EC345DED44952FE9F1F9B.gr7.us-west-2.eks.amazonaws.com",
-            "--deploy-mode", "cluster",
-            "--name", dag_name,
-            "--conf", "spark.kubernetes.namespace=airflow",
-            "--conf", "spark.kubernetes.authenticate.driver.serviceAccountName=airflow-irsa",
-            "--conf", "spark.kubernetes.container.image.pullSecrets=ecr-pull-secret",
-            "--conf", "spark.hadoop.fs.s3a.aws.credentials.provider=com.amazonaws.auth.WebIdentityTokenCredentialsProvider",
-            "--conf", "spark.executor.instances=1",
-            "--conf", "spark.executor.memory=512m",
-            "--conf", "spark.executor.cores=1",
-            "--conf", f"spark.ui.proxyBase=/spark-ui/{dag_name}",
-            "--conf", f"spark.kubernetes.driver.label.spark-ui-selector={dag_name}",
-            "--conf", "spark.kubernetes.executor.deleteOnTermination=true",
-            "--conf", "spark.sql.sources.partitionOverwriteMode=dynamic",
-            "--conf", f"spark.kubernetes.container.image={spark_image}",
-            "--conf", f"spark.kubernetes.driver.container.image={spark_image}",
-            "s3a://creatz-aim-members/kbjin/monitoring_logs_to_parquet_daily.py",
-            "--start-date", "{{ ds }}",
-            "--end-date", "{{ ds }}"
-        ],
-        get_logs=True,
-        is_delete_operator_pod=True,
-        service_account_name="airflow-irsa",
-        image_pull_secrets=[k8s.V1LocalObjectReference(name="ecr-pull-secret")],
-        container_resources=V1ResourceRequirements(
-            requests={"memory": "1Gi", "cpu": "500m"},
-            limits={"memory": "2Gi", "cpu": "1000m"},
-        )
-    )
+    @task()
+    def run_spark_job(**context):
+        return KubernetesPodOperator(
+            task_id="run_spark_submit_s3_script_daily",
+            name="spark-submit-s3-script-daily",
+            namespace="airflow",
+            image=spark_image,
+            cmds=["/opt/spark/bin/spark-submit"],
+            arguments=[
+                "--master", "k8s://https://BFDDB67D4B8EC345DED44952FE9F1F9B.gr7.us-west-2.eks.amazonaws.com",
+                "--deploy-mode", "cluster",
+                "--name", dag_name,
+                "--conf", "spark.kubernetes.namespace=airflow",
+                "--conf", "spark.kubernetes.authenticate.driver.serviceAccountName=airflow-irsa",
+                "--conf", "spark.kubernetes.container.image.pullSecrets=ecr-pull-secret",
+                "--conf", "spark.hadoop.fs.s3a.aws.credentials.provider=com.amazonaws.auth.WebIdentityTokenCredentialsProvider",
+                "--conf", "spark.executor.instances=1",
+                "--conf", "spark.executor.memory=512m",
+                "--conf", "spark.executor.cores=1",
+                "--conf", f"spark.ui.proxyBase=/spark-ui/{dag_name}",
+                "--conf", f"spark.kubernetes.driver.label.spark-ui-selector={dag_name}",
+                "--conf", "spark.kubernetes.executor.deleteOnTermination=true",
+                "--conf", "spark.sql.sources.partitionOverwriteMode=dynamic",
+                "--conf", f"spark.kubernetes.container.image={spark_image}",
+                "--conf", f"spark.kubernetes.driver.container.image={spark_image}",
+                "s3a://creatz-aim-members/kbjin/monitoring_logs_to_parquet_daily.py",
+                "--start-date", context["ds"],
+                "--end-date", context["ds"]
+            ],
+            get_logs=True,
+            is_delete_operator_pod=True,
+            service_account_name="airflow-irsa",
+            image_pull_secrets=[k8s.V1LocalObjectReference(name="ecr-pull-secret")],
+            container_resources=V1ResourceRequirements(
+                requests={"memory": "1Gi", "cpu": "500m"},
+                limits={"memory": "2Gi", "cpu": "1000m"},
+            ),
+            dag=dag,
+        ).execute(context=context)
 
-    spark_submit
+    run_spark_job()
