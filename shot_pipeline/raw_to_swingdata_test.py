646bcc1636ee4aebf5a758963d421adfb25a4010
kyoungbin jin <kbjin@mycreatz.com>
2025-07-11 18:29:35 +0900

    fix

--- shot_pipeline/raw_to_swingdata_test.py
+++ shot_pipeline/raw_to_swingdata_test.py
@@ -119,9 +119,9 @@ def raw_to_swingdata_daily_dag():
             "sparkVersion":        "3.5.3",
             "image":               spark_image,
             "imagePullPolicy":     "Always",
-            "mainApplicationFile": "s3a://creatz-airflow-jobs/raw_to_parquet/scripts/run_raw_to_parquet.py",
+            "mainApplicationFile": "local:///opt/spark/jobs/raw_to_parquet/run_raw_to_parquet.py",
             "deps":{
-                "pyFiles": ["s3a://creatz-airflow-jobs/raw_to_parquet/zips/dependencies.zip"]
+                "pyFiles": ["local:///opt/spark/jobs/raw_to_parquet/dependencies/"]
                 },
             "arguments": [
                 "--start-date", date_template,
