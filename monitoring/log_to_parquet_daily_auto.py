8c850c7dd23d68d715dc291ae4f798fb5b8eb8d8
kyoungbin jin <kbjin@mycreatz.com>
2025-11-03 09:34:31 +0900

    fix

--- monitoring/log_to_parquet_daily_auto.py
+++ monitoring/log_to_parquet_daily_auto.py
@@ -9,7 +9,7 @@ api_server  = "k8s://https://BFDDB67D4B8EC345DED44952FE9F1F9B.gr7.us-west-2.eks.
 
 default_args = {
     "owner": "airflow",
-    "retries": 1,
+    "retries": 3,
     "retry_delay": timedelta(minutes=10),
 }
 
